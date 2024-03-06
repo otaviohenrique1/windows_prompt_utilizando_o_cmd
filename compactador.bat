cfeb18a6f2bf135db3f490b97081a211ca5aaf44
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-03-06 19:23:15 -0300

    commit 5

--- compactador.bat
+++ compactador.bat
@@ -1,3 +1,4 @@
 @echo off
 echo compactando arquivos
 tar -cf notas.zip *.xml > erros.txt
+IF %ERRORLEVEL% NEQ 0 (echo "Erro na execução do script")
---
+++ prompt/bin/script2.bat
@@ -0,0 +1,9 @@
+@echo off 
+rem limpando a tela
+cls
+set /p nome=Digite seu nome=
+set /p email=Digite seu e-mail=
+pause
+echo Pressione [enter] para continuar
+echo ................................
+echo Seu nome é %nome% e seu e-mail %email%
